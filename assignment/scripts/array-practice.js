b0f1ac7f8cbc95cd38b91ecaa65c1506243563a0
Adam Bergstrom <abx1138@gmail.com>
2022-11-07 16:40:51 -0600

    finished first STRETCH question

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -82,13 +82,23 @@ console.log('The food items are now: ', foodArray);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
-
+let firstFood = foodArray.shift();
+let secondFood = foodArray.shift();
+foodArray.unshift('chili dog');
+foodArray.unshift(firstFood);
+console.log('Food array with new second food item: ', foodArray);
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+
+// Note: This is reverse order not reverse alphabetical.
+console.log('Reverse food array: ', foodArray.reverse());
+
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+
+
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
