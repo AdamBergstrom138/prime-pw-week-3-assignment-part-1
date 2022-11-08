088493fbabd31a3044af1a6a12b31313be8f3a7f
Adam Bergstrom <abx1138@gmail.com>
2022-11-08 12:56:41 -0600

    completed questions through 4.g

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -1,103 +1,130 @@
 console.log('****** Array Practice *******');
 
 // 1. Creating arrays
+
 console.log('--- 1. Practice creating arrays ---');
 
 // Example: Array of animals
+
 let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
+
 let foodArray = ['pizza', 'jerky', 'perogies', 'ramen'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
+
 console.log('Adam\'s favorite foods are: ', foodArray);
 
 // 2. Array.length - tells you how many items are in the array
+
 console.log('--- 2. Length of an array ---');
 
 // Example: How many animals are in the array? 
+
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
+
 console.log('Number of foods: ', foodArray.length);
 
 // 3. Accessing array items
+
 console.log('--- 3. Accessing items in an array ---');
 
 // Example: Log the first animal from the array using it's array index
+
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
+
 console.log('Second food item is: ', foodArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
+
 console.log('Last food item is: ', foodArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
+
 console.log('Finding the last food item using array length: ', foodArray[foodArray.length - 1]);
 
 // 4. Adding & Removing Array Items
+
 console.log('--- 4. Adding and removing array items ---');
 
 // Example: Add an animal to the end using Array.push
+
 animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
+
 foodArray.push('cheese curds');
 console.log('Added a new food item: ', foodArray);
 
 // Example: Remove the last animal by using Array.pop
+
 let removedAnimal = animalArray.pop();
 console.log('Removed the last animal', removedAnimal);
 console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
+
 let removedFood = foodArray.pop();
 console.log('Removed the last food item: ', removedFood);
 console.log('The food items are now: ', foodArray)
 
 // Example: Add an animal to the beginning using Array.unshift
+
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
+
 foodArray.unshift('fries');
 console.log('Added a new food item to the beginning: ', foodArray);
 
 // Example: Remove the first animal using Array.shift
+
 removedAnimal = animalArray.shift();
 console.log('Removed the first animal', removedAnimal);
 console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
+
 removedFood = foodArray.shift();
 console.log('Removed the first food item: ', removedFood);
 console.log('The food items are now: ', foodArray);
+
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+
 let firstFood = foodArray.shift();
 let secondFood = foodArray.shift();
 foodArray.unshift('chili dog');
 foodArray.unshift(firstFood);
 console.log('Food array with new second food item: ', foodArray);
+
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
 
-// Note: This is reverse order not reverse alphabetical.
-console.log('Reverse food array: ', foodArray.reverse());
-
+foodArray.sort();  //sorting into alphabetical order. 
+//console.log(foodArray);
+foodArray.reverse(); //reversing the order of the array.
+console.log('Reverse alphabetical order:', foodArray);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
 
+foodArray.reverse();  //reversing the string back to original format.
+console.log('Array to string with and between:', foodArray.join(' and '));
 
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
@@ -105,3 +132,4 @@ console.log('Reverse food array: ', foodArray.reverse());
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+
